c09d612925dc0c6f225cdb94eee31781312f42ab
NortonV <93251769+NortonV@users.noreply.github.com>
2025-12-05 20:00:40 +0100

    Deleted cash files

--- src/tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-
-        self.font = pygame.font.SysFont("arial", 16, bold=True)
-        self.large_font = pygame.font.SysFont("arial", 30, bold=True)
-        
-        self.grid = Grid(w, h)
-        self.bridge = Bridge()
-        self.toolbar = Toolbar(w, h)
-        self.graph = GraphOverlay(20, h - 250, 400, 150)
-        self.prop_menu = PropertyMenu(w, h)
