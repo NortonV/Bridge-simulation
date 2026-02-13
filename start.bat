9e1d4c086aaf963c8267e71f0eed11d96f2e5ed9
NortonV <93251769+NortonV@users.noreply.github.com>
2026-02-13 22:07:05 +0100

    Updated the downloader

--- start.bat
+++ start.bat
@@ -8,7 +8,7 @@ set "PY_URL=https://www.python.org/ftp/python/3.11.5/python-3.11.5-amd64.exe"
 
 echo.
 echo ===================================================
-echo      Ixchel Hidja - Smart Launcher
+echo      Ixchel Hidja
 echo ===================================================
 echo.
 
@@ -23,8 +23,6 @@ if %errorlevel% neq 0 (
     :: If that fails, try the Windows 'py' launcher
     py --version >nul 2>&1
     if !errorlevel! equ 0 (
-        echo [INFO] Standard 'python' command not found, but 'py' launcher detected.
-        echo [INFO] Using 'py' launcher...
         set "PYTHON_CMD=py"
     ) else (
         :: If both fail, THEN we install
@@ -35,18 +33,18 @@ if %errorlevel% neq 0 (
 goto :CHECK_DEPS
 
 :INSTALL_PYTHON
-echo [STATUS] Python not detected.
-echo [ACTION] Downloading Python...
+echo [STATUS] Python nem érzékelve.
+echo [ACTION] Python Letöltése...
 curl -o python_installer.exe "%PY_URL%"
 if not exist python_installer.exe (
-    echo [ERROR] Download failed. Check internet.
+    echo [ERROR] Sikertelen letöltés.
     pause
     exit /b
 )
-echo [ACTION] Installing... (Wait for the window to finish)
+echo [ACTION] Letöltés... (Ne zárja be a terminált)
 python_installer.exe /passive InstallAllUsers=0 PrependPath=1 Include_pip=1
 del python_installer.exe
-echo [INFO] Restarting launcher...
+echo [INFO] Újraindítás...
 start "" "%~f0"
 exit /b
 
@@ -57,7 +55,7 @@ exit /b
 :: We use the detected command (python or py) to check libraries
 "%PYTHON_CMD%" -c "import pygame; import numpy" >nul 2>&1
 if %errorlevel% neq 0 (
-    echo [STATUS] Installing game libraries...
+    echo [STATUS] Könyvtárak letöltése...
     :: We use -m pip to ensure we install to the correct Python
     "%PYTHON_CMD%" -m pip install -r %REQ_FILE%
 )
@@ -65,7 +63,7 @@ if %errorlevel% neq 0 (
 :: -----------------------------------------------------
 :: 3. LAUNCH GAME
 :: -----------------------------------------------------
-echo [STATUS] Launching game...
+echo [STATUS] Szimuláció indítása...
 echo ===================================================
 "%PYTHON_CMD%" %MAIN_SCRIPT%
 
